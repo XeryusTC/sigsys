7954a66b093a63b888c00b6ff704f20a13e2fe07
XeryusTC <armadillo@onenetbeyond.org>
2015-11-20 09:54:19 +0100

    finish up to 1b

--- lab1/gensinusoid.m
+++ lab1/gensinusoid.m
@@ -2,7 +2,5 @@ function x = gensinusoid (A, f, phi, t1, t2, Fs)
 	dx = 1/Fs;
 	domain = t1:dx:t2;
 	omega = 2*pi*f;
-	x = cos(omega*domain+phi);
-
-	x = x * A;
+	x = A * cos(omega*domain+phi);
 endfunction
