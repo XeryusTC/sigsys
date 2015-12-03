74484125e28ee0ffe6071f547ee8b8dbd77e8d21
Yaroslav Shkarupa <yaroslav@shkarupa.name>
2015-12-03 14:23:23 +0100

    2f

--- lab1/microphone.m
+++ lab1/microphone.m
@@ -1,12 +1,12 @@
 function x = microphone(micx, micy, srcx, srcy, srcf, srcA, srcphi, Fs)
 
-	signals = zeros([length(srcx) Fs*0.5+1]);
+	signals = zeros([length(srcx) Fs+1]);
 	for i = 1:length(srcx)
 		shift = sqrt((micy - srcy(i))^2 + (micx - srcx(i))^2) / 343.2; % Calculate distance and time shift of signal 
-		signals(i,:) = gensinusoid(srcA(i), srcf(i), srcphi(i) - shift*2*srcf(i)*pi, 0, 0.5, Fs); % Time shift is added to signal
+		signals(i,:) = gensinusoid(srcA(i), srcf(i), srcphi(i) - shift*2*srcf(i)*pi, 0, 1, Fs); % Time shift is added to signal
 	endfor
 
-	if size(signals) == [1 Fs*0.5+1]
+	if size(signals) == [1 Fs+1]
 		x = signals;
 	else
 		x = sum(signals); % Summing signals from different sources
--- lab1/position.m
+++ lab1/position.m
@@ -1,8 +1,11 @@
 function pos = position (dmic, Fs, sig1, sig2, sig3)
-	t = 60/Fs
+	t = 0:1/Fs:1;
 	
-	d1 = shift(sig2, sig1)
-	d2 = shift(sig3, sig2)
+	d1 = shift(sig1, sig2);
+	l1 = t(d1);
+	d2 = shift(sig2, sig3);
+	l2 = t(d2);
 
-	pos = dtoa(dmic, d1, d2);
+	pos = dtoa(dmic, l2, l1);
+
 endfunction
