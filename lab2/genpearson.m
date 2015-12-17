cc8d88689e321ae868d03ad6949240759e247c4c
XeryusTC <armadillo@onenetbeyond.org>
2015-12-17 20:46:13 +0100

    Make generic pearson correlator faster (hopefully)

--- lab2/genpearson.m
+++ lab2/genpearson.m
@@ -2,18 +2,35 @@ function corr = genpearson(x, y)
 assert(length(x) >= length(y), "y can not be larger than x")
 
 N = length(x);
+l = length(y);
 y = y(end:-1:1);
-x = [x(N-length(y)+1:end) x]
+x = [x(N-l+1:end) x];
 corr = zeros(1, N);
 
 ybar = mean(y);
-ynorm = sqrt(sum((y .- ybar).^2));
+ycorr = y .- ybar;
+ynorm = sqrt(sum((ycorr).^2));
+if ynorm == 0
+	return
+endif
+
+# Keep track of the running average using these
+xlow = 0;
+xhigh = sum(x(1:l));
+slice = x(1:l);
+
 for i = 1:N
-	slice = x(i:i+length(y)-1);
-	xbar = mean(slice);
-	slice = slice .- xbar;
-	corr(i) = conv(slice, y.-ybar, shape='valid')(1);
-	corr(i) = corr(i) / (sqrt(sum(slice.^2)) * ynorm);
+	xbar = (xhigh - xlow) / l;
+	xmean = slice .- xbar;
+	xnorm = sqrt(sum(xmean .^ 2));
+	if xnorm != 0
+		corr(i) = conv(xmean, ycorr, shape='valid');
+		corr(i) = corr(i) / (xnorm * ynorm);
+	endif
+
+	xlow = xlow + x(i);
+	xhigh = xhigh + x(i+l);
+	slice = [slice(2:end) x(i+l)];
 endfor
 
 endfunction
